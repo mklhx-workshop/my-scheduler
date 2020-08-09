29584764e7d705c3d8111247596fc7f31544f69b
MkLHX <mickael.lehoux@gmail.com>
2020-08-09 10:35:09 +0200

    remove commented code

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,21 +1,3 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import {
-//   AdminGuesser,
-//   hydraDataProvider,
-//   hydraSchemaAnalyzer,
-// } from '@api-platform/admin';
-
-// const Admin = () => (
-//   <AdminGuesser
-//     // Use your custom data provider or resource schema analyzer
-//     dataProvider={hydraDataProvider('https://127.0.0.1:8000')}
-//     schemaAnalyzer={hydraSchemaAnalyzer()}
-//   />
-// );
-
-// ReactDOM.render(<Admin />, document.getElementById('api-platform-admin'));
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { HydraAdmin } from '@api-platform/admin';
